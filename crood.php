1990f060e457f6c6edf7ebaf79acfe42bfb84901
webchimp <rodrigo.tejero@thewebchi.mp>
2015-07-01 10:38:44 -0500

    Norm & Crood lives

---
+++ crood.php
@@ -0,0 +1,229 @@
+<?php
+	class CROOD {
+
+		protected $table;
+		protected $table_fields;
+		protected $update_fields;
+		protected $singular_class_name;
+		protected $plural_class_name;
+
+		public $id;
+
+		# MetaModel
+		protected $meta_id;
+		protected $meta_table;
+
+		function init() {
+
+			$this->id = 					0;
+
+			$this->table = 					null;
+			$this->table_fields = 			null;
+			$this->update_fields = 			null;
+			$this->singular_class_name = 	null;
+			$this->plural_class_name = 		null;
+
+			# MetaModel
+			$this->meta_table = 			null;
+			$this->meta_id = 				null;
+		}
+
+		# Create & Update
+		function save() {
+
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = false;
+
+			$table_fields = querify($this->table_fields);
+			$bind_fields = querify($this->table_fields, 'bind');
+			$param_fields = querify($this->update_fields, 'param');
+
+			try {
+				# Create or update user
+				$sql = "INSERT INTO {$this->table} ({$table_fields})
+						VALUES ({$bind_fields})
+						ON DUPLICATE KEY UPDATE {$param_fields}";
+
+				$stmt = $dbh->prepare($sql);
+
+				foreach($table_fields as $table_field) {
+					$stmt->bindValue(":{$table_field}", $this->$table_field);
+				}
+
+				$stmt->execute();
+
+				if (! $this->id && $dbh->lastInsertId() ) {
+					$this->id = $dbh->lastInsertId();
+				}
+
+				$ret = $this->id;
+
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+			}
+			return $ret;
+		}
+
+		/**
+		 * Delete model
+		 * @return boolean True on success, False otherwise
+		 */
+		function delete() {
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = false;
+
+			try {
+				$sql = "DELETE FROM {$this->table} WHERE id = :id";
+				$stmt = $dbh->prepare($sql);
+				$stmt->bindValue(':id', $this->id);
+				$stmt->execute();
+				$ret = true;
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+			}
+			return $ret;
+		}
+
+		function __toString() {
+			return json_encode($this);
+		}
+
+		/**
+		 * Querify the fields passed (implodes)
+		 * @param  array $fields  Array with field list
+		 * @return string         String with all the fields imploded for querying
+		 */
+		public function querify($fields, $action = false) {
+			if ($action == 'bind') {
+				foreach ($fields as $field) {
+					$field = ":{$field}";
+				}
+			}
+
+			else if($action == 'param') {
+
+				foreach ($fields as $field) {
+					$field = "{$field} = :{$field}";
+				}
+			}
+
+			return implode(', ', $fields);
+		}
+
+		/*  __  ___     __        __  ___          __     __
+		   /  |/  /__  / /_____ _/  |/  /___  ____/ /__  / /
+		  / /|_/ / _ \/ __/ __ `/ /|_/ / __ \/ __  / _ \/ /
+		 / /  / /  __/ /_/ /_/ / /  / / /_/ / /_/ /  __/ /
+		/_/  /_/\___/\__/\__,_/_/  /_/\____/\__,_/\___/_/
+		                                                 */
+
+		function getMeta($name, $default = '') {
+
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = $default;
+
+			try {
+				$sql = "SELECT value FROM {$this->meta_table} WHERE {$this->meta_id} = :id AND name = :name";
+				$stmt = $dbh->prepare($sql);
+				$stmt->bindValue(':id', $this->id);
+				$stmt->bindValue(':name', $name);
+				$stmt->execute();
+				if ( $row = $stmt->fetch() ) {
+					$ret = @unserialize($row->value);
+					if ($ret === false) {
+						$ret = $row->value;
+					}
+				}
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+			}
+			return $ret;
+		}
+
+		function getMetas() {
+
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = array();
+			try {
+				$sql = "SELECT name, value FROM {$this->meta_table} WHERE {$this->meta_id} = :id";
+				$stmt = $dbh->prepare($sql);
+				$stmt->bindValue(':id', $this->id);
+				$stmt->execute();
+				$metas = $stmt->fetchAll();
+
+				foreach($metas as $meta) {
+
+					$ret[$meta->name] = $meta->value;
+				}
+
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+			}
+			return (object) $ret;
+		}
+
+		function updateMeta($name, $value) {
+
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = false;
+			if ( is_array($value) || is_object($value) ) {
+				$value = serialize($value);
+			}
+			try {
+				$sql = "INSERT INTO {$this->meta_table} (id, {$this->meta_id}, value, name) VALUES (0, :meta_id, :value, :name) ON DUPLICATE KEY UPDATE value = :value";
+				$stmt = $dbh->prepare($sql);
+				$stmt->bindValue(':meta_id', $this->id);
+				$stmt->bindValue(':value', $value);
+				$stmt->bindValue(':name', $name);
+				$stmt->execute();
+				if ( $dbh->lastInsertId() ) {
+					$ret = true;
+				}
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+			}
+			return $ret;
+		}
+
+		function updateMetas($metas) {
+
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = false;
+
+			if( $metas && is_array($metas) ) {
+
+				try {
+					$dbh->query('START TRANSACTION');
+
+					$sql = "INSERT INTO {$this->meta_table} (id, {$this->meta_id}, value, name) VALUES (0, :meta_id, :value, :name) ON DUPLICATE KEY UPDATE value = :value";
+					$stmt = $dbh->prepare($sql);
+
+					foreach( $metas as $name => $value ) {
+
+						if ( is_array($value) || is_object($value) ) {
+							$value = serialize($value);
+						}
+
+						$stmt->bindValue(':meta_id', $this->id);
+						$stmt->bindValue(':value', $value);
+						$stmt->bindValue(':name', $name);
+						$stmt->execute();
+					}
+					$dbh->query('COMMIT');
+					$ret = true;
+
+				} catch (PDOException $e) {
+					log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+				}
+			}
+
+			return $ret;
+		}
+	}
+?>
---
+++ norm.php
@@ -0,0 +1,186 @@
+<?php
+	class NORM {
+
+		protected static $table;
+		protected static $table_fields;
+		protected static $singular_class_name;
+		protected static $plural_class_name;
+
+		/**
+		 * The al'mighty magic __callStatic function
+		 * @param  string $method  Name of the method called
+		 * @param  array $params   Non asociative array with the params from called methos
+		 * @return mixed           Return values depending on the method called
+		 */
+		public static function __callStatic($method, $params) {
+
+			if ( substr($method, 0, 5) == 'getBy' ) {
+
+				$options = array();
+				$field = substr($method, 5);
+				$options['conditions'] = "{$field} = '{$params[0]}'";
+
+				if ( isset($params[1]) && is_array($params) )
+					$options = array_merge($options, $params[1]);
+
+				return self::get($options);
+			}
+
+			else if ( substr($method, 0, 7) == 'getLike' ) {
+
+				$options = array();
+				$field = substr($method, 7);
+				$options['conditions'] = "{$field} LIKE '%{$params[0]}%'";
+
+				if ( isset($params[1]) && is_array($params) )
+					$options = array_merge($options, $params[1]);
+
+				return self::get($options);
+			}
+
+			else if ( substr($method, 0, 5) == 'allBy' ) {
+
+				$options = array();
+				$field = substr($method, 5);
+				$options['conditions'] = "{$field} = '{$params[0]}'";
+
+				if ( isset($params[1]) && is_array($params) )
+					$options = array_merge($options, $params[1]);
+
+				return self::all($options);
+			}
+
+			else if ( substr($method, 0, 7) == 'allLike' ) {
+
+				$options = array();
+				$field = substr($method, 7);
+				$options['conditions'] = "{$field} LIKE '%{$params[0]}%'";
+
+				if ( isset($params[1]) && is_array($params) )
+					$options = array_merge($options, $params[1]);
+
+				return self::all($options);
+			}
+		}
+
+		/**
+		 * Querify the fields passed (implodes)
+		 * @param  array $fields  Array with field list
+		 * @return string         String with all the fields imploded for querying
+		 */
+		public static function querify($fields, $action = false) {
+			if ($action == 'bind') {
+				foreach ($fields as $field) {
+					$field = ":{$field}";
+				}
+			}
+
+			else if($action == 'params') {
+
+				foreach ($fields as $field) {
+					$field = "{$field} = :{$field}";
+				}
+			}
+
+			return implode(', ', $fields);
+		}
+
+		/**
+		 * Return the number of elements depending on the conditions
+		 * @param  string $id  Condition for the counting query
+		 * @return string      Number of counted elements
+		 */
+		public static function count($conditions = 1) {
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = 0;
+
+			# Generals
+			$table = static::$table;
+			$class_name = static::$plural_class_name;
+
+			try {
+				$sql = "SELECT COUNT(*) AS total FROM {$table} WHERE {$conditions};";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$row = $stmt->fetch();
+				$ret = $row->total;
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::count(): {$e->getMessage()}" );
+			}
+			return $ret;
+		}
+
+		/**
+		 * Retrieve one element from the database depending the conditions
+		 * @param  array $options  List of options intended to modify the query behavior
+		 * @return array           Array with User objects, False on error
+		 */
+		public static function get( $options = array() ) {
+
+			$ret = false;
+
+			$options['show'] = 1;
+			$rows = self::all($options);
+
+			if($rows) {
+
+				$ret = array_shift($rows);
+			}
+			return $ret;
+		}
+
+		/**
+		 * Retrieve all the elements from the database depending the conditions
+		 * @param  array $options  List of options intended to modify the query behavior
+		 * @return array           Array with User objects, False on error
+		 */
+		public static function all( $options = array() ) {
+			global $site;
+			$dbh = $site->getDatabase();
+			$ret = array();
+
+			#Generals
+			$table = static::$table;
+			$table_fields = static::$table_fields;
+			$class_name = static::$plural_class_name;
+			$query_fields = static::querify(get_item($options, 'query_fields', $table_fields));
+
+			#Default variables
+			$show =			get_item($options, 'show', 1000);
+			$page =			get_item($options, 'page', 1);
+			$sort =			get_item($options, 'sort', 'asc');
+			$by =			get_item($options, 'by', 'id');
+			$conditions =	get_item($options, 'conditions', '');
+			$pdoargs =		get_item($options, 'pdoargs', array());
+
+			$offset = $show * ($page - 1);
+
+			# Sanity checks
+			$by =		in_array($by, $table_fields) ? $by : false;
+			$sort =		in_array( $sort, array('asc', 'desc') ) ? $sort : false;
+			$sort =		strtoupper($sort);
+			$offset =	is_numeric($offset) ? $offset : false;
+			$show =		is_numeric($show) ? $show : false;
+
+			if ($by === false || $sort === false || $offset === false || $show === false) {
+
+				log_to_file('Parameter Error: by, sort, offset or show not well defined. (Line' . __FILE__ . ')', 'norm');
+				return $ret;
+			}
+
+			$conditions = $conditions ? "WHERE {$conditions}" : '';
+
+			try {
+				$sql = "SELECT {$query_fields} FROM {$table} {$conditions} ORDER BY {$by} {$sort} LIMIT {$offset}, {$show};";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$stmt->setFetchMode(PDO::FETCH_CLASS, static::$singular_class_name, $pdoargs);
+				$ret = $stmt->fetchAll();
+			} catch (PDOException $e) {
+				log_to_file( "Database error: {$e->getCode()} (Line {$e->getLine()}) in {$class_name}::all(): {$e->getMessage()}", 'norm' );
+			}
+			return $ret;
+		}
+	}
+?>
